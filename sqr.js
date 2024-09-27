f43ce5236370bdfacfa658516848d3980df4d050
Pomazoff <stpomazov@mail.ru>
2024-09-27 21:40:04 +0300

    first commit

---
+++ sqr.js
@@ -0,0 +1,20 @@
+
+var userName = prompt("Введите сюда своё имя", "Введите имя");
+
+if (userName) {
+    document.write("Привет, " + userName + "!");
+} else if (userName == "Введите имя"){
+    alert("Вы не ввели имя...");
+    document.write("Привет, анонимус!");
+}
+else{
+    document.write("Привет, анонимус!");
+}
+
+var restart = confirm("Начать заново?");
+
+if (restart == true) {
+    alert("Не надоело?");
+} else {
+    alert("Ну и правильно!");
+}
